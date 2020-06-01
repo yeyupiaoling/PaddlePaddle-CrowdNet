d7cfaca483bd759e7f0cd3de91600d03e4b8a091
yeyupiaoling <yeyupiaoling@foxmail.com>
2020-06-01 14:59:42 +0800

    fix infer bug

--- train.py
+++ train.py
@@ -1,28 +1,28 @@
 import os
 import shutil
-import numpy as np
 import paddle
 import paddle.fluid as fluid
 from model import crowd_deconv_without_bn, dilations_cnn
 from reader import train_set
 
-np.set_printoptions(threshold=np.inf)
 
 # 是否使用GPU
 use_cuda = True
-# 预测模型保存；路径
-infer_model_path = 'infer_model/'
 # 模型参数保存路径
-persistables_model_path = 'persistables_model/'
+model_path = 'models/'
 # 训练轮数
-epochs_sum = 200
+epochs_sum = 400
+# Batch大小
+batch_size = 8
 
+# 定义输入层
 images = fluid.layers.data(name='images', shape=[3, 640, 480], dtype='float32')
 label = fluid.layers.data(name='label', shape=[1, 80, 60], dtype='float32')
 img_num = fluid.layers.data(name='img_num', shape=[1], dtype='float32')
+
+# 创建CrowdNet网络模型
 net_out1 = crowd_deconv_without_bn(images)
 net_out2 = dilations_cnn(images)
-
 concat_out = fluid.layers.concat([net_out1, net_out2], axis=1)
 concat_out = fluid.layers.batch_norm(input=concat_out, act='relu')
 conv_end = fluid.layers.conv2d(input=concat_out, num_filters=1, filter_size=1)
@@ -30,14 +30,14 @@ map_out = fluid.layers.resize_bilinear(conv_end, out_shape=(80, 60))
 sum_ = fluid.layers.reduce_sum(map_out, dim=[1, 2, 3])
 sum_ = fluid.layers.reshape(sum_, [-1, 1])
 
+# 定义损失函数
 cost = fluid.layers.square_error_cost(input=map_out, label=label)
 avg_cost = fluid.layers.mean(cost)
-
 loss_ = fluid.layers.abs(fluid.layers.elementwise_sub(sum_, img_num))
 loss = fluid.layers.mean(fluid.layers.elementwise_div(loss_, img_num))
 sum_loss = loss + avg_cost * 6e5
 
-optimizer = fluid.optimizer.Adam(learning_rate=0.0001)
+optimizer = fluid.optimizer.Adam(learning_rate=0.001)
 optimizer.minimize(sum_loss)
 
 # 定义执行器
@@ -48,17 +48,17 @@ exe.run(fluid.default_startup_program())
 feeder = fluid.DataFeeder(feed_list=[images, label, img_num], place=place)
 
 # 定义reader
-train_reader = paddle.batch(reader=train_set(), batch_size=4)
+train_reader = paddle.batch(reader=train_set(), batch_size=batch_size)
 
 # 加载训练模型
-if persistables_model_path is not None and os.path.exists(persistables_model_path):
+if model_path is not None and os.path.exists(model_path):
     def if_exist(var):
-        if os.path.exists(os.path.join(persistables_model_path, var.name)):
+        if os.path.exists(os.path.join(model_path, var.name)):
             print('loaded: %s' % var.name)
-        return os.path.exists(os.path.join(persistables_model_path, var.name))
+        return os.path.exists(os.path.join(model_path, var.name))
 
 
-    fluid.io.load_vars(exe, persistables_model_path, main_program=fluid.default_main_program(), predicate=if_exist)
+    fluid.io.load_vars(exe, model_path, main_program=fluid.default_main_program(), predicate=if_exist)
 
 # 开始训练
 for epochs in range(epochs_sum):
@@ -68,15 +68,10 @@ for epochs in range(epochs_sum):
                                                           fetch_list=[sum_loss, label, sum_, img_num])
 
         if batch_id % 100 == 0:
-            print('Pass:%d, Batch:%d, Cost:%0.5f, predict_sum:%0.5f, label_sum:%0.5f' % (
+            print('Pass:%d, Batch:%d, Cost:%0.5f, predict_sum:%0.2f, label_sum:%0.2f' % (
                 epochs, batch_id, train_cost[0], predict_sum[0], label_sum[0]))
 
     # 保存模型
-    shutil.rmtree(infer_model_path, ignore_errors=True)
-    os.makedirs(infer_model_path)
-    fluid.io.save_inference_model(infer_model_path, [images.name], [sum_], exe)
-
-    # 保存模型
-    shutil.rmtree(persistables_model_path, ignore_errors=True)
-    os.makedirs(persistables_model_path)
-    fluid.io.save_persistables(exe, persistables_model_path)
+    shutil.rmtree(model_path, ignore_errors=True)
+    os.makedirs(model_path)
+    fluid.io.save_persistables(exe, model_path)
